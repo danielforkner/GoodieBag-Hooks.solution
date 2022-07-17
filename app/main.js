0b775b10b413adadd202c5914578f5f01a5828c2
danielforkner <daniel.forkner@gmail.com>
2022-07-17 12:36:53 -0400

    dependencies

--- app/main.js
+++ app/main.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
 import store from "./store";
 import Root from "./components/root";
 
 const root = createRoot(document.getElementById("main"));
-root.render(  <Provider store={store}>
-  <Root />
-</Provider>);
+root.render(
+  <Provider store={store}>
+    <Root />
+  </Provider>
+);
